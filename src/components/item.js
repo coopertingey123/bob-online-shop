fd5ff443f7feb773ddebb40ad5fdbb517a5528e4
coopertingeydevpipeline <cooper@devpipeline.com>
2022-08-07 16:12:43 -0600

    style changes

--- src/components/item.js
+++ src/components/item.js
@@ -1,21 +1,33 @@
+import { useState } from "react";
+
+import ItemModal from "./itemModal";
+
 export default function SingleItem({ details }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
-    <div className="item-wrapper">
+    <div className="item-wrapper" onClick={() => setModalOpen((o) => !o)}>
       <div className="image">
         <img src={details.image} alt="item here" />
       </div>
       <div className="details-wrapper">
         <div className="details">
-          <div className="name">Name: {details.name}</div>
-          <div className="cost">Price: ${details.cost}</div>
-          <div className="manufacturer">
-            Manufacturer: {details.manufacturer}
-          </div>
+          <div className="detail">Name: {details.name}</div>
+          <div className="detail">Price: ${details.cost}</div>
+          <div className="detail">Manufacturer: {details.manufacturer}</div>
         </div>
         <div className="add-to-cart">
           <button> + Add to shopping cart</button>
         </div>
       </div>
+
+      {modalOpen ? (
+        <ItemModal
+          details={details}
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+        />
+      ) : null}
     </div>
   );
 }
---
+++ src/components/itemModal.js
@@ -0,0 +1,52 @@
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+export default function ItemModal({ modalOpen, details }) {
+  return (
+    <Modal
+      isOpen={modalOpen}
+      style={{
+        overlay: {},
+        content: {
+          position: "relative",
+          width: "70%",
+          height: "90%",
+          top: "3%",
+          left: "15%",
+          borderRadius: "10px",
+          backgroundColor: "white",
+          padding: "10px",
+          overflow: "visible",
+          border: "3px solid #ffa69e",
+        },
+      }}
+    >
+      <div className="single-item-wrapper">
+        <div className="image">
+          <img src={details.image} alt="item here" />
+        </div>
+        <div className="details-wrapper">
+          <div className="detail">Name: {details.name}</div>
+          <div className="detail">Price: ${details.cost}</div>
+          <div className="detail">Manufacturer: {details.manufacturer}</div>
+          {details.rating ? (
+            <div className="detail">Rating: {details.rating}</div>
+          ) : null}
+          {details.description ? (
+            <div className="detail">Description: {details.description}</div>
+          ) : null}
+          {details.size ? (
+            <div className="detail">Size: {details.size}</div>
+          ) : null}
+          {details.other ? (
+            <div className="detail">Other: {details.other}</div>
+          ) : null}
+        </div>
+        <div className="buttons">
+          <button> + Add to shopping cart</button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
